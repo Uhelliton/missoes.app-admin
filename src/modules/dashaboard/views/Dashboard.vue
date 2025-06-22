209bba32f788766defb7a679f71fce06788da525
Uhelliton Andrade <uhelliton@uol.com.br>
2025-06-22 20:43:51 -0300

    feat(dashboard): integration widget summary date and chat daily Evangelism

--- src/modules/dashaboard/views/Dashboard.vue
+++ src/modules/dashaboard/views/Dashboard.vue
@@ -1,26 +1,19 @@
 <template>
   <b-row>
     <SummaryWidget  />
-    <MonthlyAvgIncome />
+    <SummaryDailyEvangelismChat />
   </b-row>
-<!--  <b-row class="justify-content-center">-->
-<!--    <PopularProducts />-->
-<!--    <Customers />-->
-<!--  </b-row>-->
-  <b-row class="justify-content-center">
+  <b-row class="justify-content-center" style="display: none">
     <TopSelling />
     <CategoriesData />
     <RecentsOrder />
   </b-row>
 </template>
 <script setup lang="ts">
-import StatWidgets from "../components/StatWidgets.vue";
-import MonthlyAvgIncome from "../components/MonthlyAvgIncome.vue";
-import PopularProducts from "../components/PopularProducts.vue";
-import Customers from "../components/Customers.vue";
+
 import TopSelling from "../components/TopSelling.vue";
 import CategoriesData from "../components/CategoriesData.vue";
 import RecentsOrder from "../components/RecentsOrder.vue";
 import SummaryWidget from "../components/SummaryWidget.vue";
-import DefaultLayout from "@/presentation/layouts/DefaultLayout.vue";
+import SummaryDailyEvangelismChat from "@/modules/dashaboard/components/SummaryDailyEvangelismChat.vue";
 </script>
